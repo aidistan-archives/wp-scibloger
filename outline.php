67a9231d34aa82b442d490130f897e4aad699c00
Aidi Stan <aidistan@Aidi-X1.Fedora>
2013-11-28 06:27:41 +0800

    Outline finished

--- outline.php
+++ outline.php
@@ -7,38 +7,46 @@ class SciBloger_Outline {
   const PAGE_TITLE = 'SciBloger/Outline';
   const MENU_SLUG  = 'scibloger_outline_page';
 
+  var $outline_content = "";
+
   function __construct() {
-    // Main function
-    //if ( get_option( ScienceBlogHelper::OPTION_OUTLINE ) == 'on' )
-    add_action( 'wp_head', array($this, 'insert_head' ), 10);
-    add_action( 'get_sidebar', array($this, 'insert_outline'));
+    if ( get_option( ScienceBlogHelper::OPTION_OUTLINE ) == 'on' )
+      add_action( 'wp', array($this, 'init') );
   }
 
-  function insert_head() {
-    ?>
-    <style type="text/css">
-      #scibloger_outline_trigger{
-        position:fixed;
-        padding: 20px;
-        right: 0;
-        top: 61.8%;
-      }
-      #scibloger_outline_box{
-        position:fixed;
-        padding: 20px;
-        right: 0;
-        top: 61.8%;
-      }
-    </style>
-    <?php
+  function init() {
+    if (is_single()) {
+      add_action( 'wp_enqueue_scripts', array($this, 'register_style') );
+      add_filter( 'the_content', array($this, 'add_content_anchors'), 500);
+      add_action( 'wp_footer', array($this, 'add_outline') );
+    }
   }
 
-  function insert_outline() {
+  function register_style() {
+    wp_register_style( 'scibloger_outline', plugins_url( 'aidi-wp-scibloger/outline.css' ), array(), false );
+    wp_enqueue_style( 'scibloger_outline', array(), false );
+  }
+
+  function add_content_anchors($content) {
+    $heads = array();
+    preg_match_all('/<h(\d)[^>]*>(.*)<\/h\d>/isU', $content, $mat);
+    for($i = 0; $i < count($mat[0]); $i++) {
+      array_push($heads, '<a href="#scibloger_outline_a'.$i.'" class="scibloger_outline_h'.$mat[1][$i].'">'.$mat[2][$i].'</a><br />');
+      $content = str_replace($mat[0][$i], $mat[0][$i].'<a name="scibloger_outline_a'.$i.'"></a>', $content);
+    }
+    $this -> outline_content = "\n<!-- SciBloger Outline start-->\n".join("\n", $heads)."\n<!-- SciBloger Outline end-->\n";
+    return $content;
+  }
+
+  function add_outline() {
     ?>
-    <dit id="scibloger_outline_trigger">
-    </div>
-    <div id="scibloger_outline_box" >
-      Aidi is here.
+    <div id="scibloger_outline_wrapper">
+      <table><tr><td width="40px">
+        <div class="trigger">&lt;</div>
+      </td><td>
+        <div class="content"><?php echo $this -> outline_content; ?>
+        </div>
+      </td></tr></table>
     </div>
     <?php
   }
